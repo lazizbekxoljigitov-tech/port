655c6114371c8f75d759e97cbec5d9ccf417df7e
Ismingiz <lazizbekxoljigitov@gmail.com>
2026-04-24 14:31:49 +0500

    kot

---
+++ script.js
@@ -0,0 +1,19 @@
+gsap.from(".navbar", {
+  y: -100,
+  opacity: 0,
+  duration: 1,
+});
+
+gsap.from(".banner__text", {
+  opacity: 0,
+  y: 100,
+  duration: 2,
+  delay: 2,
+});
+
+gsap.from(".hero__img", {
+  opacity: 0,
+  y: 100,
+  duration: 1,
+  delay:1
+});
